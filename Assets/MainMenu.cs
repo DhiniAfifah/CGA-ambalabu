e01a26be579b4befc25b48c7b75efe8470764b93
owin40 <godwinwoisiri40@gmail.com>
2024-11-16 23:49:40 +0700

    feat: main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private GameObject mainMenu;
+    private GameObject optionMenu;
+    private GameObject loading;
+
+    public AudioSource buttonSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenu = GameObject.Find("MainMenuCanvas");
+        optionMenu = GameObject.Find("OptionCanvas");
+        loading = GameObject.Find("LoadingCanvas");
+
+        mainMenu.GetComponent<Canvas>().enabled = true;
+        optionMenu.GetComponent<Canvas>().enabled = false;
+        loading.GetComponent<Canvas>().enabled = false;
+    }
+
+    public void StartBtn()
+    {
+        loading.GetComponent<Canvas>().enabled = true;
+        mainMenu.GetComponent<Canvas>().enabled = false;
+        buttonSound.Play();
+        SceneManager.LoadScene("test");
+    }
+
+    public void OptionBtn()
+    {
+        buttonSound.Play();
+        mainMenu.GetComponent<Canvas>().enabled = false;
+        optionMenu.GetComponent <Canvas>().enabled = true;
+    }
+
+    public void ExitBtn()
+    {
+        buttonSound.Play();
+        Application.Quit();
+    }
+
+    public void ReturnToMainMenuBtn()
+    {
+        buttonSound.Play();
+        mainMenu.GetComponent <Canvas>().enabled = true;
+        optionMenu.GetComponent< Canvas>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
